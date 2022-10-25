dbca3f2a07805a8bc8b5d8dad1fbc923805af99a
Ethan J Forster <ethoforster@gmail.com>
2022-10-24 22:22:50 -0500

    started task 1
    -created empty thread class for task 1
    -created function for task 1
    -added random integers for N & M
    -created access matrix and populated it

--- project02_C00231110/src/com/company/Main.java
+++ project02_C00231110/src/com/company/Main.java
@@ -1,8 +1,69 @@
 package com.company;
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.concurrent.Semaphore;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Task1();
     }
+
+    public static void Task1(){
+        //initializing variables
+        Random r = new Random();
+        // this is N
+        int numDomains = r.nextInt(5) + 3;
+        System.out.println("Domain Count: " + numDomains);
+
+        //This is M
+        int numObjects = r.nextInt(5) + 3;
+        System.out.println("Object Count: " + numObjects);
+
+        //creating and populating access matrix
+        int access;
+        String [][] matrix = new String [numDomains][numDomains + numObjects];
+        for (int i = 0; i < numDomains; i++){
+            for (int j = 0; j < numObjects; j++){
+                access = r.nextInt(4);
+                switch (access) {
+                    case(0):
+                        matrix[i][j] = "   ";
+                        break;
+                    case(1):
+                        matrix[i][j] = "R  ";
+                        break;
+                    case(2):
+                        matrix[i][j] = "W  ";
+                        break;
+                    case(3):
+                        matrix[i][j] = "R/W";
+                        break;
+                }
+            }
+        }
+        for (int i = 0; i < numDomains; i++){
+            for (int j = numObjects; j < numDomains + numObjects; j++){
+                access = r.nextInt(2);
+                if (j == i+numObjects){
+                    access = 0;
+                }
+                if (access == 0){
+                    matrix[i][j] = "     ";
+                } else {
+                    matrix[i][j] = "allow";
+                }
+            }
+        }
+        int count = 1;
+        for (String[] row : matrix) {
+            System.out.print("D" + count + ": ");
+            System.out.println(Arrays.toString(row));
+            count++;
+        }
+
+    }
+
 }
---
+++ project02_C00231110/src/com/company/MyThreadMatrix.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.Random;
+
+public class MyThreadMatrix extends Thread{
+
+
+    public MyThreadMatrix(){
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
